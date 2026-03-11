5394f6fbe1d322fcbd9727562d3abb5b225499e0
jiangfeng <610545517@qq.com>
2026-03-11 09:27:58 +0800

    spring ai alibaba

---
+++ backend-java/ai-chat-ali/src/main/java/ff/pro/aichatali/AiChatAliApplication.java
@@ -0,0 +1,13 @@
+package ff.pro.aichatali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatAliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiChatAliApplication.class, args);
+    }
+
+}
