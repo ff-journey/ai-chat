13c9bc8e6caf08d884869f0451f839cb2570facc
ff-journey <610545517@qq.com>
2026-03-23 01:10:22 +0800

    rag demo

--- backend-java/ai-chat-ali/src/main/java/ff/pro/aichatali/AiChatAliApplication.java
+++ backend-java/ai-chat-ali/src/main/java/ff/pro/aichatali/AiChatAliApplication.java
@@ -2,8 +2,9 @@ package ff.pro.aichatali;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiEmbeddingAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {OpenAiEmbeddingAutoConfiguration.class})
 public class AiChatAliApplication {
 
     public static void main(String[] args) {
